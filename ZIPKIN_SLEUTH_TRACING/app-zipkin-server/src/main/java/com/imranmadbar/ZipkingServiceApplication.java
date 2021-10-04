b66bf0f54647776af9865101db92972e2e1b4fa8
MD_IMRAN_HOSSAIN <imranmadbar@gmail.com>
2021-10-04 18:30:42 +0600

    add zipkin server

---
+++ ZIPKIN_SLEUTH_TRACING/app-zipkin-server/src/main/java/com/imranmadbar/ZipkingServiceApplication.java
@@ -0,0 +1,17 @@
+package com.imranmadbar;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkingServiceApplication.class, args);
+    }
+
+}
